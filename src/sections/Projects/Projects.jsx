c7b81cd1bab658e9897ffd4e7aad1c49bd30ed74
aditya-ast <adisingh7478@gmail.com>
2024-06-23 00:57:28 +0530

    image update

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,10 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
-import freshBurger from "../../assets/fresh-burger.png";
-import hipsster from "../../assets/hipsster.png";
-import fitLift from "../../assets/fitlift.png";
+import js from "../../assets/js.png";
+import powerbi from "../../assets/powerbi.png";
+import html_css from "../../assets/html_css.png";
+import mern from "../../assets/mern.png";
+import react from "../../assets/react.png";
+import ai from "../../assets/ai.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -11,69 +13,69 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/aditya-ast"
-          h3="Viberr"
-          p="Streaming App"
+          src={mern}
+          link="https://www.godigitify.com/"
+          h3="Godigitify Nexus"
+          p="An IT Company Website"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/aditya-ast"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={powerbi}
+          link="https://github.com/aditya-ast/PowerBI_Amazon_Dashboard.git"
+          h3="Amazon Product Dashboard"
+          p="PowerBI Dashboard for Amazon Products"
         />
         <ProjectCard
-          src={hipsster}
-          link="https://github.com/aditya-ast"
-          h3="Hipsster"
-          p="Glasses Shop"
+          src={mern}
+          link="https://clickmaster.netlify.app/"
+          h3="Click Master"
+          p="Photographer Booking Website"
         />
         <ProjectCard
-          src={fitLift}
+          src={powerbi}
+          link="https://github.com/aditya-ast/PowerBI_Virat_Kohli_Dashboard.git"
+          h3="Virat Kohli Dashboard"
+          p="PowerBI Performance Dashboard"
+        />
+        <ProjectCard
+          src={html_css}
+          link="https://aditya-ast.github.io/c4_educare_website/"
+          h3="C4 Educare"
+          p="Education Consultant Website"
+        />
+        <ProjectCard
+          src={js}
+          link="https://aditya-ast.github.io/Pokemon_Sound/"
+          h3="Pokemon Sound"
+          p="Pokemon Sound Generator"
+        />
+        <ProjectCard
+          src={react}
+          link="https://onepetroof-m997tnh3u-adityas-projects-0b55d2ff.vercel.app/"
+          h3="OnePetRoof"
+          p="Pet Website Frontend"
+        />
+        <ProjectCard
+          src={html_css}
+          link="https://aditya-ast.github.io/My_Protfolio_html-css/"
+          h3="My Portfolio"
+          p="Portfolio Website"
+        />
+        <ProjectCard
+          src={react}
           link="https://github.com/aditya-ast"
-          h3="FitLift"
+          h3="mern"
           p="Fitness App"
         />
         <ProjectCard
-          src={fitLift}
+          src={js}
           link="https://github.com/aditya-ast"
-          h3="FitLift"
+          h3="mern"
           p="Fitness App"
         />
         <ProjectCard
-          src={fitLift}
+          src={powerbi}
           link="https://github.com/aditya-ast"
-          h3="FitLift"
-          p="Fitness App"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/aditya-ast"
-          h3="FitLift"
-          p="Fitness App"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/aditya-ast"
-          h3="FitLift"
-          p="Fitness App"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/aditya-ast"
-          h3="FitLift"
-          p="Fitness App"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/aditya-ast"
-          h3="FitLift"
-          p="Fitness App"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/aditya-ast"
-          h3="FitLift"
+          h3="mern"
           p="Fitness App"
         />
       </div>
