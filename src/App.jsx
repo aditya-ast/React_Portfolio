24c89cd87fb9b494e5c9c2fd1c627006d1142ab6
aditya-ast <adisingh7478@gmail.com>
2024-08-09 15:05:03 +0530

    ts particle added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css';
+import Design from './component/Design.jsx';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
+
 function App() {
   return (
     <>
+    
       <Hero />
       <Projects />
       <Skills />
       <Contact />
       <Footer />
+      <Design/>
     </>
   );
 }
